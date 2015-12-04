3a6ee04dbed1bd3662e7c87d27f4229fd9f2321f
Johan Kasperi <johan@kasperi.se>
2015-12-04 12:34:17 +0100

    updated style.css

--- public/scripts/controllers/GUIController.js
+++ public/scripts/controllers/GUIController.js
@@ -3,8 +3,6 @@
 var GUIController = {
 	init: function() {
 		$('#gui').css("display", "block");
-		$('#gui').removeClass("fade-in");
-		$('#gui').addClass("fade-out");
 
 		$("#exitgame-button").click(function(e) {
 			e.preventDefault();
